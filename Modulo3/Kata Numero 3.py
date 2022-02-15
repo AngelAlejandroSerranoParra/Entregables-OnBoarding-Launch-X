77bbf79f876f3a0a29c5b3698a7dc26b45ab5679
Angel Alejandro Serrano Parra <99061297+AngelAlejandroSerranoParra@users.noreply.github.com>
2022-02-15 00:08:33 -0600

    Add files via upload

---
+++ Modulo3/Kata Numero 3.py
@@ -0,0 +1,13 @@
+asteroide = 49
+if asteroide > 25:
+    print('¡Alerta! ¡Un asteroide se acerca a velocidades peligrosas!')
+else:
+    print('¡Sigue con tu día!')
+    
+    asteroide = 19
+if asteroide > 20:
+    print('Look up! ¡Hay una luz mágica en el cielo!')
+elif asteroide == 20:
+    print('Look up! ¡Hay una luz mágica en el cielo!')
+else:
+    print('¡Nada que ver aquí!')
